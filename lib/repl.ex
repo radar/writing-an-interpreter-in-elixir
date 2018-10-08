d193911532d7cd86c31fac471731862c744290d3
Ryan Bigg <me@ryanbigg.com>
2018-10-09 10:10:00 +1100

    REPL now works with new tokenizer

--- lib/repl.ex
+++ lib/repl.ex
@@ -1,26 +1,20 @@
 defmodule Interpreter.REPL do
-  @prompt ">>"
-
   def start do
     IO.puts "Hello! This is the Monkey programming language!"
     IO.puts "Feel free to type in commands"
+    prompt()
   end
 
   def prompt do
     input = IO.gets ">> "
-    {:ok, lexer} = Lexer.start_link(input)
-    show_tokens(lexer)
+    input |> Lexer.tokenize |> show_token
 
     prompt()
   end
 
-  defp show_tokens(lexer) do
-    lexer |> Lexer.next_token |> show_token(lexer)
-  end
-
-  def show_token(%Token.EOF{}, _lexer), do: IO.puts("EOF")
-  def show_token(token, lexer) do
+  def show_token([%Token.EOF{} | []]), do: IO.puts("EOF")
+  def show_token([token | rest]) do
     token |> IO.inspect
-    lexer |> show_tokens
+    show_token(rest)
   end
 end
